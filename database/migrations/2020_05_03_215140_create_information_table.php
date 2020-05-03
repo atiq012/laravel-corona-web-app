b6a96692b3f751418c1157ea508f74e806cc4a63
atiq012 <43025786+atiq012@users.noreply.github.com>
2020-05-04 03:56:36 +0600

    info table add and registration file commented

---
+++ database/migrations/2020_05_03_215140_create_information_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('information', function (Blueprint $table) {
+            $table->id();
+            $table->string('thana');
+            $table->integer('infected');
+            $table->integer('new_infected');
+            $table->integer('recover');
+            $table->integer('deaths');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('information');
+    }
+}
